eb3294bf6cfbf77e7350f1fe8e062f6443dee599
notrlyrainy <69054170+notrlyrainy@users.noreply.github.com>
2024-03-03 09:27:30 -0600

    fetch Skills and Education

--- src/JobSeekerProfile.js
+++ src/JobSeekerProfile.js
@@ -15,21 +15,31 @@ var options = {
     const [data, setData] = useState(null);
   
     useEffect(() => {
-      fetch('http://localhost:8080/GetAllJobSeekers', options)
+      fetch('http://localhost:8080/GetJobseekerByID?ID=1', options)
         .then(response => response.json())
         .then(json => setData(json))
         .catch(error => console.error(error));
     }, []);
+    const request1 = fetch('http://localhost:8080/GetJobseekerByID?ID=1').then(response => response.json());
+    const request2 = fetch('http://localhost:8080/GetJobseekerEducationByID?ID=1').then(response => response.json());
+    const request3 = fetch('http://localhost:8080/GetJobseekerSkillsByID?ID=1').then(response => response.json());
+    Promise.all([request1, request2])
+      .then(([data1, data2, data3]) => {
+        console.log(data1, data2, data3);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   
     return (
       <div>
         {data ? 
         <pre>
         
-        <h1>First Name: {JSON.stringify(data[0].JobseekerFirstName, null, 2)}</h1> 
-        <h2>Middle Name: {JSON.stringify(data[0].JobseekerMiddleName, null, 2)}</h2> 
-        <h2>Last Name: {JSON.stringify(data[0].JobseekerLastName, null, 2)}</h2> 
-        <h2>Address: {JSON.stringify(data[0].JobseekerAddress, null, 2)}</h2>
+        <h1>First Name: {JSON.stringify(data1.JobseekerFirstName, null, 2)}</h1> 
+        <h2>Middle Name: {JSON.stringify(data1.JobseekerMiddleName, null, 2)}</h2> 
+        <h2>Last Name: {JSON.stringify(data1.JobseekerLastName, null, 2)}</h2> 
+        <h2>Address: {JSON.stringify(data1.JobseekerAddress, null, 2)}</h2>
         
         </pre> 
         : 'Loading...'}
